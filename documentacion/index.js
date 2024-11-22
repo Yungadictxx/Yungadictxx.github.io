2693126d6ad13c47ded82e2e8a1440b94aafb582
Yungadictxx <alecsmarin6@gmail.com>
2024-11-22 10:54:06 -0600

    Para mi vida

---
+++ documentacion/index.js
@@ -0,0 +1,4 @@
+document.getElementById("revealMessage").addEventListener("click", function () {
+  const hiddenMessage = document.getElementById("hiddenMessage");
+  hiddenMessage.style.display = "block";
+});
